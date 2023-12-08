a3442ef9bca86c562631c620510ab6e2d56e77d4
mucks <mail@mucks.dev>
2023-12-08 14:00:02 +0200

    completed day_9

--- src/day_9.rs
+++ src/day_9.rs
@@ -1,8 +1,62 @@
-fn task_1(input: &str) -> usize {
-    todo!();
+fn solve_seq(seq: &[isize], task: usize) -> isize {
+    let mut seqs = vec![seq.to_vec()];
+
+    let mut last_seq = seq.to_vec();
+    let mut new_seq = vec![];
+
+    loop {
+        new_seq = vec![];
+        for i in 0..last_seq.len() - 1 {
+            let diff = last_seq[i + 1] - last_seq[i];
+            new_seq.push(diff);
+        }
+
+        if new_seq.iter().all(|s| s == &0) {
+            break;
+        }
+
+        seqs.push(new_seq.clone());
+        last_seq = new_seq.clone();
+    }
+
+    if task == 1 {
+        for i in (1..seqs.len()).rev() {
+            *seqs[i - 1].last_mut().unwrap() += *seqs[i].last().unwrap();
+        }
+
+        let result = *seqs.first().unwrap().last().unwrap();
+
+        return result;
+    }
+
+    for i in (1..seqs.len()).rev() {
+        *seqs[i - 1].first_mut().unwrap() -= *seqs[i].first().unwrap();
+    }
+
+    let result = *seqs.first().unwrap().first().unwrap();
+
+    result
 }
-fn task_2(input: &str) -> usize {
-    todo!();
+
+fn parse_input(input: &str) -> Vec<Vec<isize>> {
+    input
+        .lines()
+        .map(|l| {
+            l.split_whitespace()
+                .filter_map(|c| c.parse().ok())
+                .collect()
+        })
+        .collect()
+}
+
+fn task_1(input: &str) -> isize {
+    let sequences = parse_input(input);
+    sequences.iter().map(|s| solve_seq(s, 1)).sum()
+}
+
+fn task_2(input: &str) -> isize {
+    let sequences = parse_input(input);
+    sequences.iter().map(|s| solve_seq(s, 2)).sum()
 }
 
 #[cfg(test)]
@@ -17,5 +71,22 @@ mod tests {
     }
 
     #[test]
-    fn test_day_9_task_1() {}
+    fn test_day_9_task_1() {
+        let sample_result = task_1(&sample_input());
+        assert_eq!(sample_result, 114);
+
+        let result = task_1(&input());
+        println!("the solution for day 9, task 1 is: {result}",);
+
+        assert_eq!(result, 1819125966);
+    }
+
+    #[test]
+    fn test_day_9_task_2() {
+        let sample_result = task_2(&sample_input());
+        assert_eq!(sample_result, 2);
+
+        let result = task_2(&input());
+        println!("the solution for day 9, task 2 is: {result}",);
+    }
 }
